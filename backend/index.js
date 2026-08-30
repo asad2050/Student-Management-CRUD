ec0abe8dae2b6bffc51f90509e474a1704171c87
Asad Patel <patelasad025@gmail.com>
2026-08-30 17:25:51 +0530

    Connected mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require("express")
 
+require("dotenv").config();
+const mongoose = require("mongoose")
+
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
-
+const DB_URL = process.env.DB_URL;
 const PORT = 8000;
 
 
+mongoose.connect(DB_URL).then(function(){
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
+    console.log("Mongodb Connected")
   })
-  
+})
