ac2d1a079a34de0b8d6c678229d6bb3a70a9327f
Asad Patel <patelasad025@gmail.com>
2026-08-31 13:37:18 +0530

    created basic login form

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
 
 const LoginPage = () => {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    console.log(email)
+    console.log(password)
+  }
   return (
-    <div>Login</div>
+    <div><h2>Login</h2>
+    <div>
+      <form>
+        <div >
+        <label>Email</label>
+        <input type='email' name='email' onChange={(e)=>{setEmail(e.target.value)}}/>      
+        </div>
+        <div>
+        <label>Password</label>
+        <input type='password' name='password' onChange={(e)=>{setPassword(e.target.value)}}/>
+        </div>
+        <div><button onClick={handleSubmit}>Login</button></div>
+      </form>
+    </div>
+    </div>
   )
 }
 
