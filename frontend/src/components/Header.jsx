376048601ea9604496cecb958b850cce0f3a5601
Asad Patel <patelasad025@gmail.com>
2026-08-31 20:50:49 +0530

    stlyed header and login page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="w-full border-b-2">
+    <nav className="w-full border-b-2 bg-amber-100 border-red-500">
       <div className="mx-auto px-8 py-4">
         <ul className="flex items-center justify-end gap-4">
           <li>
-            <Link to="/students" className="hover:font-bold">
+            <Link to="/students" className="hover:font-bold text-red-600">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/login" className="hover:font-bold">
+            <Link to="/login" className="hover:font-bold text-red-600">
               Login
             </Link>
           </li>
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,41 +1,71 @@
-import {useState} from 'react'
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
-import {login} from "../utils/authSlice"
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { login } from "../utils/authSlice";
 const LoginPage = () => {
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
 
-  async function handleSubmit(e){
-    e.preventDefault()
-    console.log(email)
-    console.log(password)
-    try{
-    const data = await axios.post("http://127.0.0.1:8000/api/login",{email,password})
-    const finalData = data.data
-    dispatch(login(finalData.accessToken))
-    } catch(err){
-      console.log(err)
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert("Enter Correct details")
+      return
+    }
+    try {
+      const data = await axios.post("http://127.0.0.1:8000/api/login", {
+        email,
+        password,
+      });
+      const finalData = data.data;
+      dispatch(login(finalData.accessToken));
+    } catch (err) {
+      console.log(err);
     }
   }
   return (
-    <div><h2>Login</h2>
-    <div>
-      <form>
-        <div >
-        <label>Email</label>
-        <input type='email' name='email' onChange={(e)=>{setEmail(e.target.value)}}/>      
-        </div>
-        <div>
-        <label>Password</label>
-        <input type='password' name='password' onChange={(e)=>{setPassword(e.target.value)}}/>
-        </div>
-        <div><button onClick={handleSubmit}>Login</button></div>
-      </form>
+    <div className="min-h-screen flex flex-col justify-start items-center pt-32">
+      <div className="w-full max-w-md bg-amber-100 p-8 rounded-md ">
+        <h2 className="text-3xl font-bold mb-4 text-center text-red-600">
+          Login
+        </h2>
+        <form >
+          <div className="mb-4">
+            <label className="text-red-600 font-bold">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="w-full px-4 py-2 border-2 border-red-300 rounded-md"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label className="text-red-600 font-bold">Password</label>
+            <input
+              type="text"
+              name="password"
+              className="w-full px-4 py-2 border-2 border-red-300 rounded-md"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="pt-2 mt-8">
+            <button
+              className="w-full  py-2 px-4  bg-orange-600 hover:bg-orange-700 cursor-pointer text-white font-bold rounded-md"
+              onClick={handleSubmit}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
